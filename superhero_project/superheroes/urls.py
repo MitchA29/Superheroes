66051dd76c5c7b9120ccb9a086c46dc45a78321e
Mitch Anderson <67846917+MitchA29@users.noreply.github.com>
2021-10-31 23:36:43 -0500

    Added detail page.

--- superhero_project/superheroes/urls.py
+++ superhero_project/superheroes/urls.py
@@ -5,6 +5,10 @@ from . import views
 
 app_name = 'superheroes'
 urlpatterns = [
-    path('',views.index, name='index')
+    path('',views.index, name='index'),
+    path('<int:hero_id>/', views.detail, name='detail'),
+    path('new/',views.create, name='create'),
+    path('<int:hero_id>/', views.edit, name='edit'),
+    path('<int:hero_id>/', views.delete, name='delete')
 ]
 
--- superhero_project/superheroes/views.py
+++ superhero_project/superheroes/views.py
@@ -1,6 +1,48 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import Superhero
 
 # Create your views here.
 def index(request):
-    return render(request,'superheroes/index.html')
+    all_heroes = Superhero.objects.all()
+    context = {
+        'all_heroes': all_heroes
+    }
+    return render(request,'superheroes/index.html', context)
+
+def detail(request, hero_id):
+    single_hero = Superhero.objects.get(pk=hero_id)
+    context = {
+        'single_hero': single_hero
+    }
+    return render(request, 'superheroes/detail.html', context)
+
+def create(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        alter_ego = request.POST.get('alter_ego')
+        primary = request.POST.get('primary')
+        secondary = request.POST.get('secondary')
+        catchphrase = request.POST.get('catchphrase')
+        new_hero = Superhero(name=name, alter_ego=alter_ego, primary_ability=primary, secondary_ability=secondary, catch_phrase=catchphrase)
+        new_hero.save()
+        return HttpResponseRedirect(reverse('superheroes:index'))
+    else:
+        return render(request, 'superheroes/create.html')
+
+def edit(request, hero_id):
+    edit_hero = Superhero.objects.get(pk=hero_id)
+    context = {
+        'edit_hero': edit_hero
+    }
+    return render(request, 'superheroes/edit.html', context)
+
+def delete(request, hero_id):
+    delete_hero = Superhero.objects.get(pk=hero_id)
+    context = {
+        'delete_hero': delete_hero
+    }
+    return render(request, 'superheroes/delete.html', context)
+
+
